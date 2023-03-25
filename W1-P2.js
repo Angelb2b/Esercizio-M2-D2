a77598ffaae44922895823bd42ef8d81243ed9a1
Angelo <125922847+Angelb2b@users.noreply.github.com>
2023-03-25 14:27:33 +0100

    Add files via upload

---
+++ W1-P2.js
@@ -0,0 +1,73 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let esercizio = "I principali datatype in javascript sono tutti gli ingredienti che ci servono per andare a creare la nostra ricetta così da arrivvare dall'input all'output. Qui troviamo i Numbers, ovvero tutto quello che ha valore numerico sia positivo che negativo fno a 2 alla cinquantatreesima, al suo interno troviamo però delle particolarità nelle espressioni, ovvero in base all espressione il risultato può essere + o - Infinito e se si va a creare un operazione matematicamente scorretta ottiano un Not A Number; le String, fanno parte di questo valore tutto ciò che è testuale, una sequenza di caratteri con l’accortezza di inglobarli tra virgolette, singole virgolette o apici diagonali. Questi possono essere di lunghezza diversa e partire adirittura da 0, qui di indicano solo le “” è viene così rappresentata la stringa vuota che non corrisponde comuqnue ad un valore; i Boolean, si tratta di un entità logica che fa ci che il rislutato dell espressione sia o vera o FontFaceSetLoadEvent; gli Undefined, qui è quando ad una variabile non andiamo ad assegnare alcun tipo di valore e i Null, rispetto ad undefined è ha una bella differenza perchè la variabile è definita ma al suo interno non contiene nulla"
+// console.log(esercizio)
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let esercizio = "Gli oggetti sono delle KeyValue o chiavi di valore che includono al loro interno sia l etichetta che gli assegniamo sia il suo valore, le possiamo definire come raccolte di coppie e per andare a definirle in linguaggio vanno inserite tra le parentesi graffe, al loro interno se tra una chiave e il suo valore voglio andare ad aggiungere più voci è sufficiente inserire una virgola. Quindi gli oggetti ci permettono di andare ad inserire  più variabili all interno di una variabile mastro, se per esempio io volessi andare a raccogliere la descrizione di una singola persona, della quale ho più dati, come età, sesso, nome e altezza, invece di inserire più variabile mi è sufficiente inserire solo una variabile generica mastro e poi al suo interno, mediante le graffe, inseirsco tutte le variabili con le caratteristiche che a me servono, creando così un gruppo di oggetti che viene visualizzato come una singola stringa"
+// console.log(esercizio)
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// console.log (12+20)
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+//let x = 12
+// console.log(x)
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let name = "Angelo"
+// console.log(name)
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let x = 12
+// console.log(x-4)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+//let name1 = "jhon"
+//let name2 = "Jhon"
+// console.log (name1)
+// console.log (name2)
+// console.log (name1===name2)
+// console.log (name1 !== name2)
